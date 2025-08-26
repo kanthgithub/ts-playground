bbcc09435b358ed72e01fad17c0d7f2b8ab378d3
lakshmi kanth <kanth.explorer@gmail.com>
2025-08-26 12:44:10 +0000

    test

--- src/heap/top-k-frequent-elements.ts
+++ src/heap/top-k-frequent-elements.ts
@@ -41,6 +41,17 @@ export const getTopKFrequentElements = (numbers: number[], kfactor: number) : nu
     return kFrequentNumbers;
 }
 
+export const getTopKFrequentElementsByMap = (data: number[], kfactor: number) : number[] => {
+
+        const frequencyMap : Map<number, number> = getNumberFrequencyMap(numbers);
+
+        const entries = Array.from(frequencyMap.entries());
+
+        const sorted_entries = entries.sort((a,b) => b[1] - a[1]);
+
+        return sorted_entries.slice(0, kfactor).map(entry => entry[0]);
+}
+
 export const getNumberFrequencyMap = (numbers: number[]) : Map<number, number> => {
 
     const frequencyMap = new Map<number, number>();
@@ -54,6 +65,9 @@ export const getNumberFrequencyMap = (numbers: number[]) : Map<number, number> =
 
 const numbers = [1,1,2,100,23,23,45,56,56,989,1,2,100, 100];
 const kfactor = 3;
-const mostFrequentELements = getTopKFrequentElements(numbers, kfactor);
+let mostFrequentELements = getTopKFrequentElements(numbers, kfactor);
+console.log(`mostFrequentELements found are: ${mostFrequentELements} `);
+console.assert(JSON.stringify(mostFrequentELements) == JSON.stringify([56,100,1]), "Failed to extract topK Frequent elements");
+mostFrequentELements = getTopKFrequentElementsByMap(numbers, kfactor);
 console.log(`mostFrequentELements found are: ${mostFrequentELements} `);
 console.assert(JSON.stringify(mostFrequentELements) == JSON.stringify([56,100,1]), "Failed to extract topK Frequent elements");
